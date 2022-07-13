1fd1d497026f77075ab52d6c7da4304092a8fe7e
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-13 17:52:51 -0300

    feat: criand o model Category e adicionando suas propriedades

---
+++ src/database/models/category.js
@@ -0,0 +1,16 @@
+const createCategory = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+         },
+         name: DataTypes.STRING
+    }, {
+        tableName: 'Categories',
+        timestamps: false
+    });
+
+    return Category
+}
+
+module.exports = createCategory;
