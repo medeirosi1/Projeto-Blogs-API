3eb38204e1c85e4086388f4b2da6b29964cda849
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-13 17:38:54 -0300

    feat: colocando a rota /id de users

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,5 +8,6 @@ const router = Router();
 router.post('/', userController.create);
 router.use(authController.validateToken);
 router.get('/', userController.list);
+router.get('/:id', userController.findById);
 
 module.exports = router;
