49778d653928877bb1a26916f4919f94c905b893
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-13 13:17:50 -0300

    feat: Criando o model user.js com as propriedades definidadas

---
+++ src/database/models/user.js
@@ -0,0 +1,21 @@
+const createUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false
+  });
+
+//   User.associate = (db) => {
+//     User.hasMany(db.Pet, { as: 'pets', foreignKey: 'userId' });
+//   }
+
+  return User;
+};
+
+module.exports = createUser;
