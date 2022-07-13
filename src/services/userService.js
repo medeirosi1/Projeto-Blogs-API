5e3caac2e5164f97d4e69eeaab018a42df22a66a
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-13 17:22:30 -0300

    feat: criando o userService fazendo as validações nas funções create e list e retornando um token caso o usuário é criado

---
+++ src/services/userService.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+const db = require('../database/models');
+const jwtService = require('./jwtService');
+
+const userService = {
+    validateBody: (data) => {
+        const schema = Joi.object({
+          displayName: Joi.string().required().min(8),
+          email: Joi.string().email().required(),
+          password: Joi.string().required().min(6),
+          image: Joi.string().required(),
+        });
+    
+        const { error, value } = schema.validate(data);
+    
+        if (error) throw error;
+        
+        return value;
+    },
+
+    list: async () => {
+      const users = await db.User.findAll({
+        attributes: { exclude: ['password'] },
+      });
+      return users;
+    },
+
+    create: async ({ displayName, email, password, image }) => {
+      const users = await db.User.findAll();
+
+      const repetido = users.some((el) => el.email === email);
+      if (repetido) {
+            const e = new Error('User already registered');
+            e.name = 'ConflictError';
+            throw e;
+      }
+
+      const user = await db.User.create({ displayName, email, password, image });
+
+      // const { password, ...userWithoutPassword } = user.toJSON();
+  
+      const token = jwtService.createToken(user.toJSON());
+  
+      return token;
+    },
+};
+
+module.exports = userService;
