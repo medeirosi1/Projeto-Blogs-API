aecb99f0ac1a69487d23d75999bf1b71b90d69e9
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-13 18:28:53 -0300

    feat: criando o service de categorie e fazendo as validações necessarias

---
+++ src/services/categorieService.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const db = require('../database/models');
+
+const categorieService = {
+    validationName: (name) => {
+        const schema = Joi.object({
+            name: Joi.string().required(),
+          });
+      
+          const { error, value } = schema.validate(name);
+      
+          if (error) throw error;
+          
+          return value;
+    },
+
+    create: async ({ name }) => {
+        const categorie = await db.Category.create({ name });
+        return categorie;
+    },
+};
+
+module.exports = categorieService;
