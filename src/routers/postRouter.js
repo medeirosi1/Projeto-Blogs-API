1be6a1dd145a381637bf042a85475e25ca1dbaa3
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-14 15:30:28 -0300

    feat: adicionando a rota get e post do postRouter

---
+++ src/routers/postRouter.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const authController = require('../controlles/authController');
+
+const postController = require('../controlles/postController');
+
+const router = Router();
+
+router.use(authController.validateToken);
+router.post('/', postController.create);
+router.get('/', postController.list);
+// router.get('/:id', userController.findById);
+
+module.exports = router;
